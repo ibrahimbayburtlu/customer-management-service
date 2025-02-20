72fa688e059bfcd94fbfc86fc4979c7c2431c253
ibrahim <ibrahimortak@ibrahim-MacBook-Pro.local>
2025-02-20 17:29:20 +0300

    CI-integration

--- src/main/java/com/getirCase/customer_management_service/consumer/CustomerEventListener.java
+++ src/main/java/com/getirCase/customer_management_service/consumer/CustomerEventListener.java
@@ -12,18 +12,20 @@ import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
-@RequiredArgsConstructor
 public class CustomerEventListener {
 
   private final ObjectMapper objectMapper;
   private final CustomerEventHandler eventHandler;
 
+  public CustomerEventListener(ObjectMapper objectMapper, CustomerEventHandler eventHandler) {
+    this.objectMapper = objectMapper.copy();
+    this.eventHandler = eventHandler;
+  }
+
   @KafkaListener(topics = "customer.events", groupId = "customer-service-group")
   public void consumeCustomerEvents(String message) {
     try {
-
       JsonNode jsonNode = objectMapper.readTree(message);
-
       String eventTypeStr = jsonNode.get("eventType").asText();
       KafkaEventType eventType = KafkaEventType.valueOf(eventTypeStr);
 
